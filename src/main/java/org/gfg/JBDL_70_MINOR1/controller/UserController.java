92f839b2e8ce67372e66d29b8647cdd0ea68426f
RAIKAR <meghanashet97@gmail.com>
2024-06-24 14:57:18 +0530

    first commit

--- src/main/java/org/gfg/JBDL_70_MINOR1/controller/UserController.java
+++ src/main/java/org/gfg/JBDL_70_MINOR1/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@ResponseBody
 @RestController
 @RequestMapping("/user")
 public class UserController {
